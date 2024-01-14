20a4b237fe7a1d604be6fe4395e8d617390608cb
Nikunj Mahawar <90510801+NikeMahawar@users.noreply.github.com>
2024-01-14 22:50:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,22 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .section__subtitle, .qualification__active, .qualification__data, .work__img, .contact__input',{interval: 200}); 
+
+//For blocking Inspect and right click 
+
+
+document.addEventListener('contextmenu', event=> event.preventDefault()); 
+document.onkeydown = function(e) { 
+if(event.keyCode == 123) { 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
+return false; 
+} 
+}
